04b9bead93e8e8751b384027125dc13a318ec684
JEFSVN <jefersonsilvahyellow@gmail.com>
2025-04-16 00:51:33 +0000

    criei as estruturas

--- meudesafio.c
+++ meudesafio.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main() {
+     int torre = 1, bispo = 1, rainha = 1;
+    
+    printf("TORRE\n");
+
+    for (torre = 1; torre <= 5; torre++) {
+        printf("%d direita\n", torre);
+    }
+      
+    printf("**torre movida cinco casas para a direita**\n");
+    
+    printf("BISPO\n");
+
+    while (bispo <= 5)
+    {
+        printf("%d cima,direita\n", bispo);
+        bispo++;
+
+    }
+
+        printf("**Bispo movido cinco casas na diagonal**\n");
+   
+        printf("RAINHA\n");
+
+        do
+        {
+            printf("%d esquerda\n", rainha);
+            rainha++;
+
+        } while (rainha <= 8);
+         
+        printf("**rainha movida oito casas para a esquerda");
+
+    
+    return 0;
+}
